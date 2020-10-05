6ca5cf9b49da0d5171888d0c3fe655dd1b809f24
Tyler Hueter <tylerhueter08@gmail.com>
2020-10-05 13:15:39 -0400

    start/stop icon for counter container

--- src/Clock/Counter.js
+++ src/Clock/Counter.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlayCircle, faStopCircle } from "@fortawesome/free-solid-svg-icons";
+
 
 
 const Counter = ({displayTime, flip}) => {
+
+    const [isRunning, setIsRunning] = useState(false);
+
+    const startStop = () => {
+        setIsRunning(!isRunning)
+    }
+
+
     
     return(
         <div className="counter">
             <h2>I am the counter</h2>
             <p className="time">{displayTime}</p>
             <button onClick={flip}>Flip</button>
+            {isRunning ? 
+                <FontAwesomeIcon
+                    icon={faStopCircle}
+                    size="2x"
+                    onClick={startStop}
+                /> :
+                <FontAwesomeIcon
+                    icon={faPlayCircle}
+                    size="2x"
+                    onClick={startStop}
+                />}
         </div>
     )
 };
