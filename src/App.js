dccbde9433a03e88e2ada71f0ef73e0fb17bdf3c
Tyler Hueter <tylerhueter08@gmail.com>
2020-10-19 21:45:48 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import MainContainer from './Clock/MainContainer';
-
+import Header from './Clock/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <MainContainer />
     </div>
   );
